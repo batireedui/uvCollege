f087715d424b0fe3974d7007f94655f4d5bbc0e2
unknown <j.batireedui@gmail.com>
2021-09-20 08:43:16 +0800

    huuchin

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import MainScreen from './src/screen/MainScreen';
-
+import IrcScreen from './src/screen/IrcScreen';
 const Stack = createNativeStackNavigator();
 
 export default function App() {
@@ -20,6 +20,7 @@ export default function App() {
         },
       }}>
         <Stack.Screen name="MainScreen" component={MainScreen} options = {{title: "Өвөрхангай ПК"}}/>
+        <Stack.Screen name="IrcScreen" component={IrcScreen} options = {{title: "Өвөрхангай ПК"}}/>
       </Stack.Navigator>
     </NavigationContainer>
   );
---
+++ src/screen/IrcScreen.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+
+const IrcScreen = () => {
+    return (
+        <View>
+            <Text>Ирцийн мэдээлэл</Text>
+        </View>
+    )
+}
+
+export default IrcScreen
+
+const styles = StyleSheet.create({})
